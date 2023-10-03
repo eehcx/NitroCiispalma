b30e6d806f74f2655b1ebb07e4b670c6c4e09261
Enoc Emanuel <eehcx.contacto@gmail.com>
2023-10-02 23:22:45 -0600

    Slice de Consulta clientes e Informes

--- src/features/index.js
+++ src/features/index.js
@@ -1,9 +1,13 @@
 // reducers.js
 import { combineReducers } from 'redux';
 import userSlice from './user/userSlice';
+import clientSlice from './client/clientSlice';
+import informSlice from './client/informSlice';
 
 const rootReducer = combineReducers({
     user: userSlice,
+    client: clientSlice,
+    inform: informSlice,
 });
 
 
