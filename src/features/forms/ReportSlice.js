96133fc110365f66ff1ac7289ec671f23750d34c
Enoc Emanuel <eehcx.contacto@gmail.com>
2023-10-11 23:15:11 -0600

    forms clice (Redux)

---
+++ src/features/forms/ReportSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    no_solicitud: '',
+    uid_package: '',
+    fecha_entrega: '',
+    fecha_recepcion: '',
+    no_muestras: '',
+    observaciones: '',
+    procedencia: '',
+    tipo_cultivo: '',
+}
+
+export const reportSlice = createSlice({
+    name: 'report',
+    initialState, 
+    reducers: {
+        update: (state, action) => {
+            const { no_solicitud, uid_package, fecha_entrega, fecha_recepcion, no_muestras, observaciones, procedencia, tipo_cultivo } = action.payload;
+            state.no_solicitud = no_solicitud;
+            state.uid_package = uid_package;
+            state.fecha_entrega = fecha_entrega;
+            state.fecha_recepcion = fecha_recepcion;
+            state.no_muestras = no_muestras;
+            state.observaciones = observaciones;
+            state.procedencia = procedencia;
+            state.tipo_cultivo = tipo_cultivo;
+        },
+        reset: () => {
+            return initialState;
+        },
+    },
+});
+
+export const { update, reset } = reportSlice.actions;
+export default reportSlice.reducer;
