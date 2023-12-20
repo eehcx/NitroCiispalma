681c5cbda1f45ea3c4c1f7e3393643ce30d11c7e
mAbimael <mabimael700@gmail.com>
2023-12-19 21:11:04 -0600

    Modulacion de funciones de guardar resultados en informes y cálculos

--- src/services/calculos.js
+++ src/services/calculos.js
@@ -13,6 +13,27 @@ import {
 } from "firebase/database";
 import { updateData } from "./services";
 
+const db = getDatabase();
+
+export const setCalculo = async (informeId) => {
+
+  const createCalculo = push(ref(db, `calculos`));
+
+  await set(createCalculo, {
+    fecha_creacion: new Date().toISOString()
+  })
+  const currentCalculo = createCalculo.key;
+
+  const informeRef = ref(db, `informes/${informeId}/informe_resultados`)
+  await update(informeRef, {uid_calculo : currentCalculo});
+
+  return 'Cálculo registrado'
+
+}
+
+
+
+
 export const setMuestras = async (calculoId, numLab) => {
   const db = getDatabase();
   const calculoRef = ref(db, "calculos/" + calculoId);
@@ -48,7 +69,7 @@ export const setMuestras = async (calculoId, numLab) => {
 };
 
 export const getMuestras = async (informeId) => {
-  const db = getDatabase();
+  
   const informeRef = ref(
     db,
     "informes/" + informeId + "/informe_resultados/0/uid_calculo"
@@ -80,65 +101,48 @@ export const updateMuestra = async (muestraId, { ...params }) => {
 };
 
 export const getResultadosMuestra = async (calculoId, numLab) => {
-    const db = getDatabase();
-    const calculoRef = ref(db, "calculos/" + calculoId);
   
-    get(calculoRef).then((snapshot) => {
-      if (snapshot.exists()) {
-        const calculosData = snapshot.val();
-        const calculosRealizados = [];
-  
-        const temp = Object.keys(calculosData).map((e) => ({ ...calculosData }));
-  
-        console.log(temp);
-      }
-    });
-  };
+  const calculoRef = ref(db, "calculos/" + calculoId);
 
-export const addResultadosMuestra = async (
+  get(calculoRef).then((snapshot) => {
+    if (snapshot.exists()) {
+      const calculosData = snapshot.val();
+      const calculosRealizados = [];
+
+      const temp = Object.keys(calculosData).map((e) => ({ ...calculosData }));
+
+      console.log(temp);
+    }
+  });
+};
+
+export const setCalculoResultadosMuestra = async (
   calculoId,
   numLab,
   calculoName,
   { ...resultadosCalculo }
 ) => {
   //Hace referencia al calculo de un informe, en la ruta del tipo de calculo que se registrará
-  const db = getDatabase();
+  
   const resultadosRef = ref(db, `calculos/${calculoId}/${calculoName}`);
 
- // Guardamos en una variable los resultados, tomando el num de laboratorio de la muestra y un objeto que contiene
- // los datos de los resultados de dicho tipo de resultado
+  // Guardamos en una variable los resultados, tomando el num de laboratorio de la muestra y un objeto que contiene
+  // los datos de los resultados de dicho tipo de resultado
   const resultados = {
     [numLab]: resultadosCalculo,
   };
 
   //Aquí evalua si tiene que actualizar o registrar los resultados
-    await get(resultadosRef).then((snapshot) => {
-      if (snapshot.exists()) {
-        update(resultadosRef, resultados);
-      } else {
-        set(resultadosRef, resultados);
-        return `Se han registrado los resultados de ${calculoName} en la muestra ${numLab}.`;
-      }
-    }).catch(error => {
-      return error
-    });
+  await get(resultadosRef).then((snapshot) => {
+    if (snapshot.exists()) {
+      update(resultadosRef, resultados);
+    } else {
+      set(resultadosRef, resultados);
+      return `Se han registrado los resultados de ${calculoName} en la muestra ${numLab}.`;
+    }
+  }).catch(error => {
+    return error
+  });
 
 };
-
-export const setResultadosMuestra = (
-  calculoId,
-  numLab,
-  {...modeloDatos}
-) => {
-
-
-  addResultadosMuestra(calculoId,
-    numLab, )
-
- Object.keys(modeloDatos).map(key => {
-    const value = modeloDatos[key]
-    console.log(key, '=>', value)
- })
-};
-
 
--- src/services/informes.js
+++ src/services/informes.js
@@ -1,54 +1,56 @@
 import { getDatabase, ref, get, push, set, orderByChild, equalTo, child, query } from 'firebase/database';
 import { updateData } from './services';
 
+const db = getDatabase();
+
 //Funcion para traer todos los informes de un cliente especifico
-export const getInformesCliente = async(clientId) =>{ 
-    const db = getDatabase();
-    const informesRef = ref(db, 'informes') 
+export const getInformesCliente = async (clientId) => {
+
+    const informesRef = ref(db, 'informes')
 
     //Este es una consulta para filtrar los informes que tengan el uid del cliente 
     const informeQuery = query(informesRef, ...[orderByChild("uid"), equalTo(clientId)])
 
     return get(informeQuery).then((snapshot => {
-        if (snapshot.exists()){
+        if (snapshot.exists()) {
             const informesCliente = snapshot.val();
             return informesCliente
         }
-        else{
+        else {
             return 'No existen informes registrados a ese cliente...'
         }
     }))
-    .catch((err) => {throw err} )  
+        .catch((err) => { throw err })
 }
 
 // Trae los datos de un informe seleccionado
-export const getInforme = async(informeId) => {
-    const db = getDatabase();
+export const getInforme = async (informeId) => {
+
     const informeRef = ref(db, 'informes/' + informeId)
-    
+
     return get(informeRef)
-    .then((async snapshot =>{
-        if (snapshot.exists()){
-            
-            const informe = snapshot.val();
-            const resultados = Object.values(informe.informe_resultados[0]);
+        .then((async snapshot => {
+            if (snapshot.exists()) {
 
-            //Imprime los resultados del informe y los datos del informe
-            const informeData = [informe, resultados]
-            return informeData;
+                const informe = snapshot.val();
+                const resultados = Object.values(informe.informe_resultados[0]);
 
-        }else{
-            console.log('No existe ese informe en la base de datos...')
-            return []
-        }
-    })
-    )
-    .catch((err =>{throw err}))
+                //Imprime los resultados del informe y los datos del informe
+                const informeData = [informe, resultados]
+                return informeData;
+
+            } else {
+                console.log('No existe ese informe en la base de datos...')
+                return []
+            }
+        })
+        )
+        .catch((err => { throw err }))
 }
-    
+
 //Funcion para crear un informe, recibe los parametros de necesarios para crear un informe
-export const setInforme = async({uid_cliente, no_solicitud, fecha_entrega, fecha_recepcion, uid_package, no_muestras, observaciones, procedencia, tipo_cultivo}) => {
-    const db = getDatabase() ;
+export const setInforme = async ({ uid_cliente, no_solicitud, fecha_entrega, fecha_recepcion, uid_package, no_muestras, observaciones, procedencia, tipo_cultivo, }) => {
+
     const createInforme = await push(ref(db, 'informes'));
     const currentInformeId = createInforme.key;
 
@@ -65,29 +67,62 @@ export const setInforme = async({uid_cliente, no_solicitud, fecha_entrega, fecha
     }
 
     await set(ref(db, `informes/${currentInformeId}`), informeParams);
-    await setResultados(currentInformeId);
+    //await setInformeResultados(currentInformeId);
 
-    return console.log('Informe registrado exitosamente!' )
+    return console.log('Informe registrado exitosamente!')
 }
 
 //Actualizar un informe pasandole un objeto como parametro
-export const updateInforme = async ( informeId, {...params}) => {
-    updateData(`informes/${informeId}`, {...params});
+export const updateInforme = async (informeId, { ...params }) => {
+    updateData(`informes/${informeId}`, { ...params });
 }
 
-const setResultados = async (informeId, id_lab) => {
+
+export const setInformeResultados = async (informeId, arrayResultados) => {
+    const informeRef = ref(db, `informes/${informeId}`);
+
+    const informe = await get(informeRef).then(
+        informe => {
+
+            const paqueteInforme = informe.val().uid_package
+            const paqueteRef = ref(db, `paquetes/${paqueteInforme}`);
+            
+            const paquete = get(paqueteRef).then(
+                async paquete => {
+                    const paqueteAnalisis = paquete.val().analisis;
+                    const elementosPaquete = {}
+
+                    for (let i = 0; i < paqueteAnalisis.length; i++) {
+                        const elemento = paqueteAnalisis[i];
+                        elementosPaquete[elemento] = '';
+                    }
+
+
+                    await set(ref(db, `informes/${informeId}/informe_resultados`), [resultadosParams]);
+                    return console.log('Los datos fueron agregados');
+
+                }
+            )
+
+        }
+    );
+
+
+}
+
+const setInformeResultados_ = async (informeId, id_lab) => {
     //Obtener el informe y obtener al paquete que tiene para los elementos
-    const db = getDatabase();
+
     const informeRef = ref(db, `informes/${informeId}`);
 
     try {
         const informeSnap = await get(informeRef)
 
-        if(informeSnap.exists()){
-            const informeData = informeSnap.val();      
+        if (informeSnap.exists()) {
+            const informeData = informeSnap.val();
             const informeIdPaquete = informeData.uid_package
 
-            const createCalculo = push(ref(db,`calculos`));
+            const createCalculo = push(ref(db, `calculos`));
 
             await set(createCalculo, {
                 fecha_creacion: new Date().toISOString()
@@ -95,17 +130,17 @@ const setResultados = async (informeId, id_lab) => {
             const currentCalculo = createCalculo.key;
 
             const paquetesRef = ref(db, `paquetes/${informeIdPaquete}`);
-            const paqueteSnap = await get(paquetesRef );
+            const paqueteSnap = await get(paquetesRef);
 
             if (paqueteSnap.exists()) {
-                const paqueteData = paqueteSnap.val() ;
-                const paqueteArray =  paqueteData.analisis;
+                const paqueteData = paqueteSnap.val();
+                const paqueteArray = paqueteData.analisis;
 
 
                 const elementosPaquete = {}
                 for (let i = 0; i < paqueteArray.length; i++) {
                     const elemento = paqueteArray[i];
-                    elementosPaquete[elemento] = '' ;
+                    elementosPaquete[elemento] = '';
                 }
 
                 const resultadosParams = {
@@ -113,12 +148,12 @@ const setResultados = async (informeId, id_lab) => {
                     uid_calculo: currentCalculo,
                     resultados: elementosPaquete
                 }
-            
-                await set(ref(db, `informes/${informeId}/informe_resultados`), [resultadosParams] );
+
+                await set(ref(db, `informes/${informeId}/informe_resultados`), [resultadosParams]);
                 return console.log('Los datos fueron agregados');
-            }       
+            }
         }
     } catch (error) {
         throw error
-    } 
+    }
 }
